2f6b9f7991c3ca777298d124ca58919ff77b46de
Ankita Patra <patra.ankita2004@gmail.com>
2024-03-10 00:12:08 +0530

    ifelse demo

---
+++ java-examples/src/IfElseDemo.java
@@ -0,0 +1,11 @@
+public class IfElseDemo {
+    public static void main(String[] args) {
+        int age=23;
+        if(age>=18) {
+            System.out.println("you can vote");
+        }
+        else{
+            System.out.println("you can not vote");
+        }
+    }
+}
