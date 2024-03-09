499e346fdc4f1717cd9f64902e4fbab72fbea8e3
Ankita Patra <patra.ankita2004@gmail.com>
2024-03-10 00:40:18 +0530

    forloop demo

---
+++ java-examples/src/EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num=25;
+        if(num % 2==0) {
+            System.out.println(" even no ");
+        }
+            else{
+                System.out.println(" odd no ");
+            }
+        }
+    }
+
---
+++ java-examples/src/ForLoop.java
@@ -0,0 +1,11 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=50;i++){
+            if(i%2==0){
+                System.out.println("all the even no from 1 to 50 = " +i);
+            }
+        }
+
+    }
+}
