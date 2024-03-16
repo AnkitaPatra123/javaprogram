979ccc1fb9c14f96ddc79da8c9172bd3166cd505
Ankita Patra <patra.ankita2004@gmail.com>
2024-03-17 00:45:21 +0530

    two decimal

--- java-examples/src/MethodDemo3.java
+++ java-examples/src/MethodDemo3.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class MethodDemo3 {
+public class MethodDemo3 {
+    public static void main(String[] args) {
+       double result= MethodDemo3.twoDecimal(81.22999999999999) ;
+        System.out.println(result);
+    }
+    static double twoDecimal(double x) {
+        String result1= String.format("%.2f", x);
+         double value=Double.parseDouble(result1);
+         return value;
+    }
 }
+
